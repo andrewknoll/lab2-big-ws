fd028c53d6a43b3ca6be5699b6f6072ed5bb66d9
Andrew <aog25.2014@gmail.com>
2020-10-25 19:39:09 +0100

    Tests passing

--- src/main/java/translator/infrastructure/DummyTranslator.java
+++ src/main/java/translator/infrastructure/DummyTranslator.java
@@ -6,8 +6,7 @@ import org.springframework.stereotype.Component;
 public class DummyTranslator extends TranslatorImpl{
 
   @Override
-  protected String translateInternal(String from, String to, String text, String encodedText) throws RuntimeException{
+  protected String translateInternal(String from, String to, String text, String encodedText){
     throw new RuntimeException("I don't know how to translate from "+from+" to "+ to + " the text '"+text+"'");
-    return null;
   }
 }
